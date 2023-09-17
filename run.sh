99158b67b66fd47ed8c1ea9d448e766d9dcdb926
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2023-09-17 15:07:33 +0530

    Update build script

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-./Configure mingw64 --cross-compile-prefix=x86_64-w64-mingw32- --prefix=./openssl-mingw-bin
+tar -xzf openssl-3.0.10.tar.gz
+cd openssl-3.0.10
+./Configure mingw64 --cross-compile-prefix=x86_64-w64-mingw32- --prefix=../openssl-mingw-bin
 make install
