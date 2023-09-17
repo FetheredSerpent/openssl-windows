cab52247aa1cfa714efb50aa6e877619d8237cf3
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2023-09-17 15:39:08 +0530

    Compress built files

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ cd openssl-3.0.10
 ./Configure mingw64 --cross-compile-prefix=x86_64-w64-mingw32- --prefix=`pwd`/openssl-mingw-bin
 make install
 
-mv openssl-mingw-bin ..
+tar -czf openssl-mingw-bin.tar.gz openssl-mingw-bin
+mkdir ../openssl-mingw-bin
+mv openssl-mingw-bin.tar.gz ../openssl-mingw-bin
